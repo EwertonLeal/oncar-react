c0609cadb550649137e2b1d912f2507388dc6da9
EwertonLeal <ewertonolivaleal6@gmail.com>
2024-02-22 11:31:02 -0300

    feat: Estilização da hero section e movendo ela uma pasta

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Box from "@mui/material/Box"
 
 import { Navbar } from "./components/Navbar"
+import { HeroSection } from "./components/HeroSection/HeroSection"
 
 export const App = () => {
   return (
     <Box>
       <Navbar />
-      
+      <HeroSection />
     </Box>
   )
 }
---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,42 @@
+import Grid from "@mui/material/Grid"
+import Typography from "@mui/material/Typography"
+
+import "./HeroSection.css";
+import { Button } from "@mui/material";
+
+export const HeroSection = () => {
+  return (
+    <Grid container spacing={2} alignItems="center">
+      <Grid item xs={12} md={6} className="headingContent">
+        <Typography variant="h2" component="h1" className="oncarHeading">
+        SEU CARRO NOVO <br/> ESTA AQUI!
+        </Typography>
+
+        <Typography variant="body1">
+          Explore nosso showroom e encontre o carro dos seus sonhos. 
+          Experimente a emoção de dirigir com segurança e conforto. 
+          Seu novo veículo espera por você aqui!
+        </Typography>
+
+        <Button
+            color="inherit"
+            variant="contained"
+            sx={{ 
+                marginTop: "40px",
+                backgroundColor: "#C92227",
+                color: "#ffffff",
+                "&:hover": {
+                    backgroundColor: "#A31115",
+                }
+            }}
+          >
+            Veja nosso catalogo
+          </Button>
+
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <img src="/hero-img.png" width="100%" alt="" />
+      </Grid>
+    </Grid>
+  );
+}
